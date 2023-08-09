a91c8a3c01af246cc5a5d4a2490a5e8babb63124
Rola Alasmar <rola.alasmar@perficient.com>
2023-08-09 13:33:21 -0500

    React practice facts page

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main(){
+    return (
+        <div className="main-content">
+            <h1 className="title">Fun Facts About React</h1>
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+       
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <nav>
+            <img className="nav--icon" src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className = "nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
