c670ccb1cf74a40b7e8ffb813645209ecb33be2c
Rola Alasmar <rola.alasmar@perficient.com>
2023-08-14 10:34:48 -0500

    added dark and light mode

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,17 @@
 import React from "react"
 
-export default function Main(){
+export default function Main(props){
     return (
-        <div className="main-content">
+        <main className={props.darkMode ? "dark" : ""}>
             <h1 className="title">Fun Facts About React</h1>
-            <ul className="list">
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
        
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react"
 
-export default function Navbar(){
+export default function Navbar(props){
     return (
-        <nav>
-            <img className="nav--icon" src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"/>
+        <nav className={props.darkMode ? "dark": ""}>
+            <img className="nav--logo_icon" src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className = "nav--title">React Course - Project 1</h4>
+            <div className="toggler">
+                <p classname="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
